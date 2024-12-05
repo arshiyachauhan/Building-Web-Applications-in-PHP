a038e681a812c3f1200101119d7055b90a887ec1
Arshiya Chauhan <119539567+arshiyachauhan@users.noreply.github.com>
2024-12-05 13:53:10 +0530

    module5

---
+++ Module5/GradedAssignment/index.php
@@ -0,0 +1,18 @@
+<title>Arshiya Chauhan PHP</title>
+<h1>Arshiya Chauhan PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Arshiya Chauhan\" is ".hash('sha256', 'Arshiya Chauhan');
+?></p>
+<pre>ASCII ART:
+	       ***
+	      *****
+	     **	  **
+	    **     **
+	   ***********
+	  *************
+	 **           **
+	**             **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
